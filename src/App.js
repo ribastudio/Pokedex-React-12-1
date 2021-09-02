368aecdcb08afafb6f1aa756c8dbae88d80f6c0c
˜Riba Jr <ribastudio@gmail.com>
2021-09-02 20:05:39 -0300

    modified files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import Button from './Components/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.nextPoke = this.nextPoke.bind(this);
+    this.state = {
+      initialClick: 0,
+    }
+  };
+
+  nextPoke() {
+    this.setState( (prev, _props) => ({
+     initialClick: prev.initialClick + 1
+    }),
+    console.log(this))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons} nextPokemon={this.state.initialClick} />
+        <Button onClickButton={ this.nextPoke } labelButton='Next Pokemon' />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Pokemon from './Pokemon';
 
+
 class Pokedex extends React.Component {
     render() {
+    const { nextPokemon } = this.props;
+    
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                {/* {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)} */}
+                <Pokemon pokemon={this.props.pokemons[nextPokemon]} />
             </div>
         );
     }
